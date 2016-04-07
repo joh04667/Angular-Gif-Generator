1f2a5d1f092cf0af050980e9e323fad14bf9b760
Kyle Johnson <joh04667@gmail.com>
2016-04-07 14:39:07 -0500

    first commit

--- app.js
+++ app.js
@@ -5,16 +5,22 @@ var app = angular.module('myApp', []);
 app.controller("GifController", function($scope, $http) {
 
   $scope.gif = {};
+  $scope.searchGif = {};
 
+ // get random gif on button press
   $scope.getGif = function() {
     $http.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC").then(function(response) {
-      $scope.buttonClicked = true;
       $scope.gif = response.data.data;
       console.log($scope.gif);
     });
-
-
   };
 
+// get searched gif
+  $scope.gifSearch = function() {
+    var randomSearch = $scope.searchText.replace(/\s/gi, "+");
+    $http.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + randomSearch).then(function(response){
+      $scope.searchGif = response.data.data;
+    });
+  };
 
 }); //gifcontroller
