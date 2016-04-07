0c5d9be0eddd8f92d4e2364a57f6b780ded4d1b8
Kyle Johnson <joh04667@gmail.com>
2016-04-07 11:23:29 -0500

    adds functionalit for button

--- app.js
+++ app.js
@@ -1,42 +1,20 @@
-$(function(){
-
-var aGif = {}
-
-function getGif() {
-$.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC").done(function(response) {
-  var aGif = response.data;
-  empty();
-   $(".gif").append("<img src=" + aGif.image_url + ">");
-   $(".Title").append("<h2>" + aGif.caption + "</h2>");
-   $(".Title").append("<h5>URL: " + aGif.url + "</h5>");
- }); // get giphy close
-} // getGif close
-
-function getRandomGif() {
-  var randomSearch = $("#rsearch").val().replace(/\s/gi, "+");
-empty();
- $.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + randomSearch).done(function(response) {
-   var aGif = response.data;
-    $(".rs").append("<img src=" + aGif.image_url + ">");
-
- });
-
-}
-
-$(".gifbutton").on("click", function(){
-getGif();
-});
-
-$("#randominput").on("submit", function(event){
-event.preventDefault();
-
-getRandomGif();
-})
+var app = angular.module('myApp', []);
 
 
-function empty() {
-  $(".gif").empty();
-  $(".title").empty();
-  $(".rs").empty();
-}
-}); // jquery close
+
+app.controller("GifController", function($scope, $http) {
+
+  $scope.gif = {};
+
+  $scope.getGif = function() {
+    $http.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC").then(function(response) {
+      $scope.buttonClicked = true;
+      $scope.gif = response.data.data;
+      console.log($scope.gif);
+    });
+
+
+  };
+
+
+}); //gifcontroller
